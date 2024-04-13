4cfcc34061e812d4bbb9f3301afa9ab2ce2991fb
Trubberov <timofey.bokach.d@gmail.com>
2024-04-13 14:11:45 +0300

    fix

--- backend/classification_service/classification_service/endpoints/__init__.py
+++ backend/classification_service/classification_service/endpoints/__init__.py
@@ -1,5 +1,5 @@
 from .health_check import api_router as health_check_router
-from .qa import api_router as get_answer_api_router
+from .classification import api_router as get_answer_api_router
 
 list_of_routes = [
     health_check_router,
