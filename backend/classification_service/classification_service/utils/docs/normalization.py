334889b50c83b033824a06fc27943b2ccb82260d
Trubberov <timofey.bokach.d@gmail.com>
2024-04-14 08:59:39 +0300

    Update normalization.py

--- backend/classification_service/classification_service/utils/docs/normalization.py
+++ backend/classification_service/classification_service/utils/docs/normalization.py
@@ -1,18 +1,15 @@
-from nltk.corpus import stopwords
+# from nltk.corpus import stopwords
 from pymorphy2 import MorphAnalyzer
 from razdel import tokenize
 
-# Загрузка стоп-слов
-stop_words = set(stopwords.words('russian'))
+# stop_words = set(stopwords.words('russian'))
 
-# Добавление стоп-слов, характерных для юридических документов
 legal_stop_words = {
     'дом', 'корпус', 'строение', 'квартира', 'офис', 'банк',
     'счет', 'бик', 'инн', 'кпп', 'номер', 'дата', 'год', 'месяц', 'день',
     'рубль', 'копейка', 'тысяча', 'миллион', 'процент', 'штука', 'кг', 'ндс', 'i', 'ii', 'iii', 'n', 'г'
 }
 
-# Инициализация морфологического анализатора
 morph = MorphAnalyzer()
 
 def normalize_text(text):
@@ -20,10 +17,10 @@ def normalize_text(text):
     tokens = [token.text.lower() for token in tokenize(text)]
 
     # Удаление стоп-слов, знаков препинания
-    filtered_tokens = [token for token in tokens if token.isalpha() and token not in stop_words]
+    # filtered_tokens = [token for token in tokens if token.isalpha() and token not in stop_words]
 
     # Лемматизация токенов
-    lemmatized_tokens = [morph.parse(token)[0].normal_form for token in filtered_tokens]
+    lemmatized_tokens = [morph.parse(token)[0].normal_form for token in tokens]
 
     # Удаление юридических стоп-слов
     filtered_lemmatized_tokens = [token for token in lemmatized_tokens if token not in legal_stop_words]
